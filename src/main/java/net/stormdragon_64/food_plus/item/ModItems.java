fa09d2a8f16f81345cd7ab0685e17d8d6baca18f
StormDragon_64 <ashtontanner13@outlook.com>
2022-11-18 17:06:50 -0800

    add ice cream - still no recipes yet

--- src/main/java/net/stormdragon_64/food_plus/item/ModItems.java
+++ src/main/java/net/stormdragon_64/food_plus/item/ModItems.java
@@ -1,5 +1,6 @@
 package net.stormdragon_64.food_plus.item;
 
+import net.minecraft.world.food.Foods;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -14,8 +15,10 @@ public class ModItems {
     public static final DeferredRegister<Item>  ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, FoodPlus.MOD_ID);
     public static final RegistryObject<Item> DONUT = ITEMS.register("donut",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(Foods.ENCHANTED_GOLDEN_APPLE)));
 
+    public static final RegistryObject<Item> VANILLA_ICE_CREAM = ITEMS.register("vanilla_ice_cream",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(Foods.ENCHANTED_GOLDEN_APPLE)));
 public static void register(IEventBus eventBus) {
     ITEMS.register(eventBus);
 }
