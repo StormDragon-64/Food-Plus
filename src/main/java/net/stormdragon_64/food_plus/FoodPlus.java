3a81289d014dfbca7bb207486333ee6a209170e0
StormDragon_64 <ashtontanner13@outlook.com>
2022-11-19 20:06:00 -0800

    add empty ice cream tub

--- src/main/java/net/stormdragon_64/food_plus/FoodPlus.java
+++ src/main/java/net/stormdragon_64/food_plus/FoodPlus.java
@@ -14,6 +14,7 @@ import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.stormdragon_64.food_plus.block.ModBlocks;
 import net.stormdragon_64.food_plus.item.ModItems;
 import org.slf4j.Logger;
 
@@ -33,6 +34,7 @@ public class FoodPlus {
         eventBus.addListener(this::setup);
 
         ModItems.register(eventBus);
+        ModBlocks.register(eventBus);
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
---
+++ src/main/java/net/stormdragon_64/food_plus/block/ModBlocks.java
@@ -0,0 +1,43 @@
+package net.stormdragon_64.food_plus.block;
+
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.stormdragon_64.food_plus.FoodPlus;
+import net.stormdragon_64.food_plus.item.ModItems;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, FoodPlus.MOD_ID);
+
+    //actual blocks
+    public static final RegistryObject<Block> EMPTY_ICE_CREAM_TUB = registerBlock("empty_ice_cream_tub",
+            () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(3f)), CreativeModeTab.TAB_DECORATIONS);
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn, tab);
+        return toReturn;
+    }
+
+    private static <T extends Block> RegistryObject<Item>
+    registerBlockItem(String name, RegistryObject<T> block, CreativeModeTab tab) {
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+
+
+
+}
