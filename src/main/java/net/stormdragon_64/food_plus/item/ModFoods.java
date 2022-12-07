050c0c37fa37481c5888be9e8f0f3793b42bcaa2
StormDragon_64 <ashtontanner13@outlook.com>
2022-12-07 08:46:23 -0800

    Finish cheese (add texture and lang entry)

--- src/main/java/net/stormdragon_64/food_plus/item/ModFoods.java
+++ src/main/java/net/stormdragon_64/food_plus/item/ModFoods.java
@@ -7,6 +7,6 @@ import net.minecraft.world.food.FoodProperties;
 public class ModFoods {
     public static final FoodProperties DONUT = (new FoodProperties.Builder()).nutrition(6).saturationMod(0.8F).fast().build();
 
-    public static final FoodProperties CHEESE = (new FoodProperties.Builder()).nutrition(2).saturationMod(0.1F).fast().build();
+    public static final FoodProperties CHEESE = (new FoodProperties.Builder()).nutrition(2).saturationMod(0.3F).fast().build();
 
 }
